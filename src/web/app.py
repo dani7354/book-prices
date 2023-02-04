a87afe7822c991610af8921bd35f10186ccef221
Daniel S. Petersen <d@stuhrs.dk>
2023-02-04 13:58:47 +0100

    Added static files for website

--- src/web/app.py
+++ src/web/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+
+@app.route('/details/{id}')
+def details():
     return '<h1>Hello from Flask</h1>'
 
 
