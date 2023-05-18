4b0daa27134a15919699fb13886fc81fb50c503c
Daniel <d@stuhrs.dk>
2023-05-18 14:57:14 +0200

    serve static content in separate container

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# start nginx service
-service nginx start &
-
 # start uwsgi application
 /usr/local/bin/uwsgi /usr/local/bookprices_web/bookprices/web/bookprices_web.ini
-
-# wait for any process to exit
-wait -n
-
-# exit with status of process that exited first
-exit $?
