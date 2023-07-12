a9690186b466329dbd0dc556acf00c67d41fdf95
Daniel <d@stuhrs.dk>
2023-07-12 20:49:16 +0200

    added test and fix

--- tests/test_update_price.py
+++ tests/test_update_price.py
@@ -1,11 +1,8 @@
 import pytest
 import requests
-import os
 from unittest.mock import MagicMock
 from bookprices.cronjob.update_prices import PriceUpdateJob
 from bookprices.shared.model.bookstore import BookInBookStore, BookStore
-from bookprices.shared.model.bookprice import BookPrice
-from bookprices.shared.model.book import Book
 from bookprices.shared.db.database import Database
 import shared
 
@@ -47,3 +44,16 @@ def test_updates_prices_creates_prices(monkeypatch, books_in_bookstore):
     args, _ = db.bookprice_db.create_prices.call_args
     assert len(args[0]) == 2
     assert args[0][0].price == 229.0
+
+
+def test_updates_prices_doesnt_save_if_no_prices_found(monkeypatch, books_in_bookstore):
+    db = Database("", "", "", "", "")
+    db.bookstore_db.get_book_stores_for_books = MagicMock(return_value={1: [], 2: []})
+    db.book_db.get_books = MagicMock(return_value=[MagicMock(), MagicMock()])
+    db.bookprice_db.create_prices = MagicMock()
+    monkeypatch.setattr(requests, "get", shared.mock_get)
+
+    job = PriceUpdateJob(db, thread_count=1)
+    job.run()
+
+    assert db.bookprice_db.create_prices.call_count == 0
