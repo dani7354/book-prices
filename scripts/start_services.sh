4d84a1e9b9bea0363a86e933e5d626358bd2d4db
Daniel <d@stuhrs.dk>
2024-01-28 10:59:36 +0100

    Static folder works for js, css, icons

---
+++ scripts/start_services.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+declare -r docker_compose_filepath=$1
+
+if [[ -z "$docker_compose_filepath" ]]; then
+  echo "Missing argument: docker-compose filepath"
+  exit 1
+fi
+
+case $2 in
+all | default)
+  docker compose -f "$docker_compose_filepath" --profile default up
+  ;;
+db)
+  docker compose -f "$docker_compose_filepath" --profile db_only up
+  ;;
+*)
+  echo "Invalid or missing argument for mode (all, db)"
+  exit 1
+  ;;
+esac
