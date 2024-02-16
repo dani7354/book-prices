e54d9acca7fd40f378ee0989f93fb395d5be746e
Daniel <d@stuhrs.dk>
2024-02-16 06:26:51 +0100

    Update sql queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,14 +1,15 @@
  -- Get the 100 latest failed price updates
- select fpu.Created, fpu.Reason, b.Title, CONCAT(bs.Url, bsb.Url) as Url, bs.Id as BookStoreId, b.Id as BookId
- from FailedPriceUpdate fpu
- inner join Book b on b.Id = fpu.BookId
- inner join BookStore bs on bs.Id = fpu.BookStoreId
- inner join BookStoreBook bsb on bsb.BookId = fpu.BookId and bsb.BookStoreId = fpu.BookStoreId
- order by fpu.created DESC limit 100;
+ SELECT fpu.Created, fpu.Reason, b.Title, CONCAT(bs.Url, bsb.Url) AS Url, bs.Id AS BookStoreId, b.Id AS BookId
+ FROM FailedPriceUpdate fpu
+ INNER JOIN Book b ON b.Id = fpu.BookId
+ INNER JOIN BookStore bs ON bs.Id = fpu.BookStoreId
+ INNER JOIN BookStoreBook bsb ON bsb.BookId = fpu.BookId AND bsb.BookStoreId = fpu.BookStoreId
+ ORDER BY fpu.created DESC
+ LIMIT 100;
 
 
 -- Get the 100 latest price updates
-SELECT b.Id, b.Isbn, b.Title, b.Author, b.Format, b.ImageUrl, b.Created, MAX(bp.Created) as PriceUpdated
+SELECT b.Id, b.Isbn, b.Title, b.Author, b.Format, b.ImageUrl, b.Created, MAX(bp.Created) AS PriceUpdated
 FROM Book b
 JOIN BookPrice bp ON b.Id = bp.BookId
 GROUP BY b.Id, b.Isbn, b.Title, b.Author, b.Format, b.ImageUrl, b.Created
