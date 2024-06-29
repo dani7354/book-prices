7cca07ba7988ee2df401fb80a534a4357a1f322d
Daniel <d@stuhrs.dk>
2024-06-29 12:23:00 +0200

    Add comment

--- tests/fake/cache.py
+++ tests/fake/cache.py
@@ -3,7 +3,7 @@ from bookprices.shared.cache.client import CacheClient
 
 class FakeCacheClient(CacheClient):
     def delete_key(self, key: str) -> None:
-        pass
+        pass  # Do nothing, cache is fake
 
     def delete_keys(self, keys: list[str]) -> None:
-        pass
+        pass  # Do nothing, cache is fake
