dba7c366d22bdb4cb0576d5d063671c17f89cbd7
Daniel <d@stuhrs.dk>
2025-04-27 09:27:44 +0200

    Add JS for delete book button

---
+++ bookprices/web/assets/js/delete_book.js
@@ -0,0 +1,31 @@
+const baseUrl = "/book";
+const redirectUrl = "/search";
+const btnDeleteBook = $("#btn-delete-book");
+
+
+$(document).ready(function () {
+    btnDeleteBook.on("click", function (e){
+        e.preventDefault();
+        if (confirm("Er du sikker på at du vil slette bogen?")) {
+            let bookId = $(e.target).data("book-id");
+            deleteBook(bookId);
+        }
+    });
+});
+
+function deleteBook(bookId) {
+    let url = `${baseUrl}/delete/${bookId}`;
+    $.ajax(url, {
+        "method": "POST",
+        "dataType": "json",
+        "data": {
+            "csrf_token": $(csrfTokenNodeId).val()
+        },
+        "success": function (data) {
+            window.location.href  = redirectUrl;
+        },
+        "error": function (error) {
+            console.log(error);
+        }
+    });
+}
