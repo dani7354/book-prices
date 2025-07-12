ba5f6097c9274254c8f07b0619dca98434be26a7
Daniel <d@stuhrs.dk>
2025-07-12 10:44:04 +0200

    Update tests

--- tests/test_price_update_service.py
+++ tests/test_price_update_service.py
@@ -21,7 +21,8 @@ def books_in_bookstore() -> dict[int, list[BookInBookStore]]:
                             image_css_selector=None,
                             price_format="\\d+",
                             has_dynamically_loaded_content=False,
-                            isbn_css_selector=None),
+                            isbn_css_selector=None,
+                            color_hex=None),
                             url="/book1"),
             BookInBookStore(book=MagicMock(),
                             book_store=BookStore(1, "BookStore 2", "https://bookstore2.com",
@@ -31,7 +32,8 @@ def books_in_bookstore() -> dict[int, list[BookInBookStore]]:
                             image_css_selector=None,
                             price_format="\\d+",
                             has_dynamically_loaded_content=False,
-                            isbn_css_selector=None),
+                            isbn_css_selector=None,
+                            color_hex=None),
                             url="/book1"),
         ]
     }
