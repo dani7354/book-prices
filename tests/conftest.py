64e65c2c072e9e81995bf406feb1f03a6ae14dd4
Daniel <d@stuhrs.dk>
2026-02-06 18:33:22 +0100

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,24 @@
+from contextlib import contextmanager
+
 import pytest
 from sqlalchemy import create_engine
-from sqlalchemy.orm import create_session
+from sqlalchemy.orm import create_session, Session
 
+from bookprices.shared.db.data_session import SessionFactory
 from bookprices.shared.db.tables import BaseModel
+
+
+class FakeSessionFactory(SessionFactory):
+
+    def __init__(self, session):
+        self._session = session
+
+    @contextmanager
+    def create_session(self):
+        yield self._session
+
+    def create_scoped_session(self) -> Session:
+        return self._session
 
 
 @pytest.fixture
@@ -14,3 +30,8 @@ def data_session():
         yield session
 
     BaseModel.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def session_factory(data_session):
+    return FakeSessionFactory(data_session)
