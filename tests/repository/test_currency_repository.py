3ca3d1911d25617a92ab3e2674cc31a95ac0e0f2
Daniel <d@stuhrs.dk>
2026-02-06 06:00:11 +0100

    Compare strings in tests instead of floats

--- tests/repository/test_currency_repository.py
+++ tests/repository/test_currency_repository.py
@@ -6,6 +6,9 @@ from bookprices.shared.db.tables import Currency
 from bookprices.shared.repository.currency import CurrencyRepository
 
 
+RATE_STR_FORMAT = ".4f"
+
+
 @pytest.fixture
 def currency_repository(data_session) -> CurrencyRepository:
     return CurrencyRepository(data_session)
@@ -20,7 +23,7 @@ def test_add_or_update_adds_currency(currency_repository: CurrencyRepository) ->
 
     assert retrieved_currency
     assert retrieved_currency.code == "USD"
-    assert retrieved_currency.rate_to_dkk == 623.90
+    assert format(retrieved_currency.rate_to_dkk, RATE_STR_FORMAT) == format(623.90, RATE_STR_FORMAT)
     assert retrieved_currency.description == "US Dollar"
 
 
@@ -41,4 +44,4 @@ def test_add_or_update_updates_exising_currencies(currency_repository: CurrencyR
     retrieved_currency = currency_repository.get_by_code(usd_currency_code)
 
     assert retrieved_currency
-    assert retrieved_currency.rate_to_dkk == 666.66
+    assert format(retrieved_currency.rate_to_dkk, RATE_STR_FORMAT) == format(666.66, RATE_STR_FORMAT)
